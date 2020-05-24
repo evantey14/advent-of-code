eea0c905b9b2ddabfc20b2e8312f52d2b91400f6
Evan Tey <evantey14@gmail.com>
2020-05-24 00:16:42 -0400

    Add lint and prettier

--- day01/solution.js
+++ day01/solution.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync('day01/input.txt')
-                 .toString()
-                 .split('\n')
-                 .filter(i => i.length > 0);
-const inputMasses = input.map(i => parseInt(i));
+const input = fs
+  .readFileSync("day01/input.txt")
+  .toString()
+  .split("\n")
+  .filter((i) => i.length > 0);
+const inputMasses = input.map((i) => parseInt(i));
 
-const getRequiredFuel = mass => Math.max(0, Math.floor(mass / 3) - 2);
+const getRequiredFuel = (mass) => Math.max(0, Math.floor(mass / 3) - 2);
 
 let currentMasses = inputMasses.map(getRequiredFuel);
 let totalFuelReq = 0;
 
-while(currentMasses.some(m => m > 0)) {
+while (currentMasses.some((m) => m > 0)) {
   totalFuelReq += currentMasses.reduce((a, b) => a + b);
   currentMasses = currentMasses.map(getRequiredFuel);
 }
