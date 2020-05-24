1de12c324eed50612e696b2768feb12cb3d9d450
Evan Tey <evantey14@gmail.com>
2020-05-24 02:24:48 -0400

    Change == to ===

--- day02/solution.js
+++ day02/solution.js
@@ -10,9 +10,9 @@ const intcode = input[0].split(",").map((i) => parseInt(i));
 
 const processInstruction = function (opcode, params, array) {
   const [index1, index2, index3] = params;
-  if (opcode == 1) {
+  if (opcode === 1) {
     array[index3] = array[index1] + array[index2];
-  } else if (opcode == 2) {
+  } else if (opcode === 2) {
     array[index3] = array[index1] * array[index2];
   }
 };
@@ -22,7 +22,7 @@ const runIntCode = function (noun, verb) {
   (copy[1] = noun), (copy[2] = verb);
   for (let i = 0; i < copy.length; i += 4) {
     const opcode = copy[i];
-    if (opcode == 99) {
+    if (opcode === 99) {
       break;
     }
     const params = copy.slice(i + 1, i + 4);
@@ -34,7 +34,7 @@ const runIntCode = function (noun, verb) {
 // We're looking for the inputs that yield 19690720
 for (let i = 0; i < 100; i++) {
   for (let j = 0; j < 100; j++) {
-    if (runIntCode(i, j) == 19690720) {
+    if (runIntCode(i, j) === 19690720) {
       console.log(i, j);
       break;
     }
