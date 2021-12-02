a98e142d1d65c69125330d0f0edfc4a63aff14ef
Evan Tey <evantey14@gmail.com>
2021-12-01 23:29:42 -0500

    Add one-line solution

---
+++ day01/fast.js
@@ -0,0 +1,9 @@
+console.log(require("fs")
+  .readFileSync("day01/input.txt")
+  .toString()
+  .split("\n")
+  .filter(i => i.length > 0)
+  .map(i => parseInt(i))
+  .reduce((prev, current, index, array) => (
+    prev + (current > array[index - 3])
+  ), 0));
