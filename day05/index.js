a97af7f9677dbfc0794e34a6deff6590d412ab3f
Evan Tey <evantey14@gmail.com>
2021-12-05 22:39:30 -0500

    Apply prettier to day05

--- day05/index.js
+++ day05/index.js
@@ -7,43 +7,41 @@ const input = fs
   .filter(i => i.length > 0);
 
 class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    equals(other) {
-        return this.x === other.x && this.y === other.y;
-    }
+  equals(other) {
+    return this.x === other.x && this.y === other.y;
+  }
 }
 
 const size = 1000;
 let field = new Array(size).fill(0).map(() => new Array(size).fill(0));
 
 for (let i = 0; i < input.length; i++) {
-    const endpoints = input[i]
-        .split(" -> ").map(i => 
-            new Point(...i.split(",").map(i => parseInt(i)))
-        );
-    console.log(endpoints);
+  const endpoints = input[i]
+    .split(" -> ")
+    .map(i => new Point(...i.split(",").map(i => parseInt(i))));
+  console.log(endpoints);
 
-    let current = endpoints[0];
-    field[current.x][current.y] += 1;
-    while (! current.equals(endpoints[1])) {
-        if (current.x < endpoints[1].x) {
-            current.x += 1;
-        } else if (current.x > endpoints[1].x) {
-            current.x -= 1;
-        }
-
-        if (current.y < endpoints[1].y) {  
-            current.y += 1;
-        } else if (current.y > endpoints[1].y) {
-            current.y -= 1;
-        }
-        field[current.x][current.y] += 1;
+  let current = endpoints[0];
+  field[current.x][current.y] += 1;
+  while (!current.equals(endpoints[1])) {
+    if (current.x < endpoints[1].x) {
+      current.x += 1;
+    } else if (current.x > endpoints[1].x) {
+      current.x -= 1;
     }
 
+    if (current.y < endpoints[1].y) {
+      current.y += 1;
+    } else if (current.y > endpoints[1].y) {
+      current.y -= 1;
+    }
+    field[current.x][current.y] += 1;
+  }
 }
 
 // console.log(field);
