27563ac5965fea4fca763e7414ba6f738bc707b9
Evan Tey <evantey14@gmail.com>
2023-12-08 14:46:24 -0500

    Add day8

---
+++ 2023/rust/src/day8.rs
@@ -0,0 +1,49 @@
+use std::{collections::HashMap, fs};
+
+pub fn main() {
+    let input = fs::read_to_string("data/day08/input.txt").unwrap();
+    let (steps, nodes_str) = input.split_once("\n\n").unwrap();
+    let mut nodes = HashMap::new();
+    for node_str in nodes_str.lines() {
+        nodes.insert(
+            node_str[0..3].to_string(),
+            (node_str[7..10].to_string(), node_str[12..15].to_string()),
+        );
+    }
+
+    println!("{}", steps);
+    println!("{:?}", nodes);
+
+    let mut current_nodes: Vec<&String> = nodes.keys().filter(|node| node.ends_with('A')).collect();
+
+    println!("Starting nodes: {:?}", current_nodes);
+
+    let mut rounds = 0;
+    while !current_nodes.iter().all(|&node| node.ends_with('Z')) {
+        for (i, step) in steps.chars().enumerate() {
+            current_nodes = current_nodes
+                .iter()
+                .map(|&node| {
+                    let (left, right) = nodes.get(node).unwrap();
+                    match step {
+                        'L' => left,
+                        'R' => right,
+                        _ => panic!("ERROR"),
+                    }
+                })
+                .collect();
+            // if current_nodes.iter().any(|&node| node.ends_with('Z')) {
+            //     println!("{:?} {}", current_nodes, rounds * steps.len() + i + 1);
+            //     break;
+            // }
+            // println!("{:?} {}", current_nodes, rounds * steps.len() + i + 1);
+        }
+
+        rounds += 1;
+        if current_nodes.iter().any(|&node| node.ends_with('Z')) {
+            println!("*{:?} {}", current_nodes, rounds * steps.len());
+        }
+        // Read logs and here were the first Zs for each position:
+        // [12083, 18827, 22199, 13207, 20513, 17141]
+    }
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -5,6 +5,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 mod hello;
 
 use std::env;
@@ -27,6 +28,7 @@ fn main() {
         5 => day5::main(),
         6 => day6::main(),
         7 => day7::main(),
+        8 => day8::main(),
         // Add more cases as needed
         _ => println!("Invalid day number."),
     }
