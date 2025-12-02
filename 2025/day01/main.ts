edd93dfd9b6b0b0ad259b3ce243c590ef72f1275
Evan Tey <evantey14@gmail.com>
2025-12-02 10:18:08 -0500

    Format day01

--- 2025/day01/main.ts
+++ 2025/day01/main.ts
@@ -7,8 +7,8 @@ const NUM_POSITIONS = 100;
 let position = 50;
 let counter = 0;
 
-lines.forEach((line,i ) => {
-let counterDelta = 0;
+lines.forEach((line, i) => {
+  let counterDelta = 0;
 
   const direction = line.substring(0, 1);
   const amount = parseInt(line.substring(1));
@@ -18,19 +18,19 @@ let counterDelta = 0;
   const oldRotation = Math.floor(position / NUM_POSITIONS);
   const newRotation = Math.floor(newPosition / NUM_POSITIONS);
 
-  if (position % NUM_POSITIONS === 0 ) {
-      counterDelta = Math.floor(amount / NUM_POSITIONS)
+  if (position % NUM_POSITIONS === 0) {
+    counterDelta = Math.floor(amount / NUM_POSITIONS);
   } else if (newPosition % NUM_POSITIONS === 0) {
-    counterDelta = Math.floor(amount / NUM_POSITIONS) + 1
+    counterDelta = Math.floor(amount / NUM_POSITIONS) + 1;
   } else if (oldRotation !== newRotation) {
     counterDelta = Math.abs(oldRotation - newRotation);
   }
 
-    console.log(
-      i,
+  console.log(
+    i,
     counter,
     counterDelta,
-    '|',
+    "|",
     position,
     oldRotation,
     position % NUM_POSITIONS,
@@ -44,7 +44,7 @@ let counterDelta = 0;
   );
 
   position = newPosition;
-  counter += counterDelta
+  counter += counterDelta;
 });
 
 console.log(counter);
