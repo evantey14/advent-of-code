e2453417a1841a44ddb2e2b0159835ce19e5751f
Evan Tey <evantey14@gmail.com>
2025-12-04 12:07:35 -0500

    Add day03

---
+++ 2025/day03/main.ts
@@ -0,0 +1,39 @@
+import { indexOf, max, sum } from "lodash-es";
+
+const data = await Deno.readTextFile("input.txt") satisfies string;
+
+const banks = data.split("\n").filter((l) => l.length > 0).map((bank) =>
+  bank.split("").map((i) => parseInt(i))
+);
+
+const BATTERIES_TO_TURN_ON = 12;
+
+function argMax(batteries: number[]): number {
+  const maxValue = max(batteries);
+  return indexOf(batteries, maxValue);
+}
+
+function getHighestJoltage(bank: number[]): number {
+  const batteryIndices = [
+    argMax(bank.slice(0, bank.length - BATTERIES_TO_TURN_ON + 1)),
+  ];
+
+  for (
+    let digitsLeft = BATTERIES_TO_TURN_ON - 1;
+    digitsLeft > 0;
+    digitsLeft--
+  ) {
+    const startingIndex = batteryIndices[batteryIndices.length - 1] + 1;
+
+    batteryIndices.push(
+      argMax(bank.slice(startingIndex, bank.length - digitsLeft + 1)) +
+        startingIndex,
+    );
+  }
+
+  return Number(batteryIndices.map((i) => bank[i]).join(""));
+}
+
+const joltages = banks.map(getHighestJoltage);
+
+console.log(sum(joltages));
